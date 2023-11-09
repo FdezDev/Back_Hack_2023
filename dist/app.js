22c626d261b90821e20c1ec55e444b2be2d01f80
Arisel-Fdez <211119@ids.upchiapas.edu.mx>
2023-11-09 02:16:41 -0600

    a

--- dist/app.js
+++ dist/app.js
@@ -16,10 +16,12 @@ const express_1 = __importDefault(require("express"));
 const signale_1 = require("signale");
 const sequelize_1 = require("./database/sequelize");
 const usersRouter_1 = require("./Users/infraestructure/usersRouter");
+const authRouter_1 = require("./auth/infraestructure/authRouter");
 const app = (0, express_1.default)();
 const signale = new signale_1.Signale();
 app.use(express_1.default.json());
 app.use('/user', usersRouter_1.UsersRouter);
+app.use('auth', authRouter_1.authRouter);
 function startServer() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
